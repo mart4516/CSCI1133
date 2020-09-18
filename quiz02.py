7bf1d81e75cf83a83c9896b10aad92d966dbc247
mart4516 <71521624+mart4516@users.noreply.github.com>
2020-09-18 15:39:26 -0500

    Create quiz02.py

---
+++ quiz02.py
@@ -0,0 +1,14 @@
+print("You have 100 dollars left.")
+print("Each cupcake costs 5 dollars.")
+buy_cupcakes = int(input("How many cupcakes are you buying?"))
+remain_money = 100 - buy_cupcakes*5
+print("You have",remain_money,"dollars left.")
+print("Each apple costs 2 dollars.")
+buy_apples = int(input("How many apples are you buying?"))
+remain_money = remain_money - buy_apples*2
+print("You have",remain_money,"dollars left.")
+guests = int(input("How many people are at the party?"))
+cupcake_allocate =
+leftover_cake = 
+apples_allocate = 
+leftover_apple =
